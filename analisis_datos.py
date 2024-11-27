cc0ecbac6c0b778a6700d4eca15721f1e855275c
Ana López <anamuyamiga@gmail.com>
2024-11-27 18:17:46 +0100

    cositas

--- analisis_datos.py
+++ analisis_datos.py
@@ -33,20 +33,45 @@ for i, var in enumerate(varianza_explicada):
     print(f'PCA {i+1}: {var*100:.2f}%')
     
 # 6. Graficar los datos en el espacio PCA
-plt.figure(figsize=(8, 6))
+def graficar_pca(df):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df['pca1'], df['pca2'], alpha=.5)
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+    plt.title('Gráfico de dispersión PCA')
+    plt.savefig('imagenes/grafico_pca.png')
+    plt.show()
+    
+
+'''plt.figure(figsize=(8, 6))
 plt.scatter(df_pca['pca1'], df_pca['pca2'], alpha=.5)
 plt.xlabel('PCA 1')
 plt.ylabel('PCA 2')
 plt.title('Gráfico de dispersión PCA')
 plt.savefig('imagenes/grafico_pca.png')
 plt.show()
-
+'''
 # Dibujar los vectores de las variables originales
 cargas = pca.components_.T * np.sqrt(pca.explained_variance_)
 cargas_df = pd.DataFrame(cargas, columns=['pca1', 'pca2'], index=df.columns)
 print("-------- CARGAS --------")
 print(cargas_df)
-plt.figure(figsize=(8, 6))
+
+def graficar_cargar_general(df, cargas):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df['pca1'], df['pca2'], alpha=.5)
+    for i in cargas.index:
+        plt.arrow(0, 0, cargas.loc[i, 'pca1'], cargas.loc[i, 'pca2'], color='r', alpha=.5, head_width=0.1)
+        plt.text(cargas.loc[i, 'pca1']*1.15, cargas.loc[i, 'pca2']*1.15, i, color='g')
+    
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+    plt.title('Gráfico de dispersión PCA con vectores de carga')
+    plt.savefig('imagenes/grafico_pca_vectores_carga.png')
+    plt.show()
+
+
+'''plt.figure(figsize=(8, 6))
 plt.scatter(df_pca['pca1'], df_pca['pca2'], alpha=.5)
 for i in cargas_df.index:
     plt.arrow(0, 0, cargas_df.loc[i, 'pca1'], cargas_df.loc[i, 'pca2'], color='r', alpha=.5, head_width=0.1)
@@ -57,9 +82,19 @@ plt.ylabel('PCA 2')
 plt.title('Gráfico de dispersión PCA con vectores de carga')
 plt.savefig('imagenes/grafico_pca_vectores_carga.png')
 plt.show()
-
+'''
 # gráfico horizontal de cargas
-plt.figure(figsize=(8, 6))
+def graficar_cargas (df, cargas, pca_num):
+    plt.figure(figsize=(8, 6))
+    plt.barh(df.columns, np.abs(cargas[f'pca{pca_num}']), alpha=.5)
+    plt.xticks(rotation=90)
+    plt.ylabel('Carga')
+    plt.title(f'Cargas de las variables en PCA {pca_num}')
+    plt.savefig(f'imagenes/grafico_cargas_pca{pca_num}.png')
+    plt.show()
+    
+
+'''plt.figure(figsize=(8, 6))
 plt.barh(df.columns, np.abs(cargas_df['pca1']), alpha=.5)
 plt.xticks(rotation=90)
 plt.ylabel('Carga')
@@ -73,7 +108,11 @@ plt.xticks(rotation=90)
 plt.ylabel('Carga')
 plt.title('Cargas de las variables en PCA 2')
 plt.savefig('imagenes/grafico_cargas_pca2.png')
-plt.show()
+plt.show()'''
 
-
+if __name__ == '__main__':
+    graficar_pca(df_pca)
+    graficar_cargar_general(df_pca, cargas_df)
+    graficar_cargas(df, cargas_df, 1)
+    graficar_cargas(df, cargas_df, 2)
 
--- modelo.py
+++ modelo.py
@@ -3,22 +3,16 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 #ignorar warnings
 import warnings
+from analisis_datos import df_scaled
 warnings.filterwarnings('ignore')
-
-#leemos los datos limpios
-df = pd.read_csv('Client_segment_limpio.csv', sep=';', encoding='latin1')
-print(df.head())
-
-# 2. Escala los datos
-scaler = StandardScaler()
-df_scaled = scaler.fit_transform(df)
 
 # 3. Calcula WCSS para diferentes valores de k
 wcss = []
-k_values = range(1, 11)  # Probar con k de 1 a 10
+k_values = range(1, 20)  # Probar con k de 1 a 20 (ponemos que son 8 o 9 los optimos)
 for k in k_values:
     kmeans = KMeans(n_clusters=k, random_state=42)
     kmeans.fit(df_scaled)
@@ -32,13 +26,14 @@ plt.xlabel('Número de Clusters (k)')
 plt.ylabel('WCSS')
 plt.xticks(k_values)
 plt.grid()
+plt.savefig('imagenes/regla_del_codo.png')
 plt.show()
 
 from sklearn.metrics import silhouette_score, silhouette_samples
 import matplotlib.cm as cm
 
 # Calcular la puntuación de silueta para diferentes valores de k
-for k in range(2, 7):  # Evalúa de 2 a 6 clusters
+for k in range(5, 10):  # Evalúa de 5 a 9 clusters
     kmeans = KMeans(n_clusters=k, random_state=42)
     labels = kmeans.fit_predict(df_scaled)
     silhouette_avg = silhouette_score(df_scaled, labels)
@@ -61,11 +56,40 @@ for k in range(2, 7):  # Evalúa de 2 a 6 clusters
     plt.xlabel("Puntuación de la Silueta")
     plt.ylabel("Clusters")
     plt.axvline(x=silhouette_avg, color="red", linestyle="--")
+    plt.savefig(f'imagenes/silueta_k{k}.png')
     plt.show()
 
 from sklearn.decomposition import PCA
+koptimo = 6
+# Kmeans con el número óptimo de clusters
+kmeans = KMeans(n_clusters=koptimo, random_state=42)
+cluster = kmeans.fit(df_scaled)
 
-# Aplicar PCA
+# Añadir los labels de cluster 
+df_scaled['Cluster'] = cluster
+print(df_scaled.head())
+
+#Obtener los índices de las columas Generacion y Campanna_anno
+gen_index = df_scaled.index('Generacion')
+campanna_index = df_scaled.index('Campanna_anno')
+
+centroide = kmeans.cluster_centers_
+
+# Visualizar el scatter plot con la edad
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x = df_scaled['Generacion'], y = df_scaled['Campanna_anno'], data = df_scaled, hue = 'Cluster', s=100, alpha=0.5, palette='viridis')
+plt.scatter(centroide[:, gen_index], centroide[:, campanna_index], s=300, c='red', label='Centroides', marker='x', edgecolors='black')
+plt.title('Segmentación de Clientes')
+plt.xlabel('Edad')
+plt.ylabel('Ingresos')
+plt.legend()
+plt.grid(True)
+plt.savefig('imagenes/segmentacion_clientes.png')
+plt.show()
+
+
+
+'''# Aplicar PCA
 pca = PCA(n_components=2)  # Cambia el número de componentes si es necesario
 df_pca = pca.fit_transform(df_scaled)
 
@@ -104,5 +128,5 @@ plt.ylabel("Componente Principal 2")
 plt.grid()
 plt.legend()
 plt.show()
-
+'''
 
